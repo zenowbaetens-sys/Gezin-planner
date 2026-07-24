39a35a6c42253ee0b0647186eafca3cbf053387a
zenowbaetens-sys <zenow.baetens@gmail.com>
2026-07-25 01:55:35 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,26 @@
+// =====================================
+// FIREBASE CONFIG
+// =====================================
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyD-oxoZI0I6J8_Cf2E6C5vQEC2pChBYb_k",
+
+authDomain: "gezin-planner.firebaseapp.com",
+
+projectId: "gezin-planner",
+
+storageBucket: "gezin-planner.firebasestorage.app",
+
+messagingSenderId: "654961420785",
+
+appId: "1:654961420785:web:5b738d004fb7163029fc50",
+
+measurementId: "G-0BTZJ5LKPX"
+
+};
+
+
+// Firebase starten
+
+firebase.initializeApp(firebaseConfig);
